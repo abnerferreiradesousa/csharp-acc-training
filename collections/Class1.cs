d7f25bbe617161ec9e66b5c39f91dd0fd5614634
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-31 20:46:25 -0300

    feat: adicionando meus treinos com LINQ e collections, graças à Deus.

---
+++ collections/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace collections;
+public class Class1
+{
+  public static void Main(string[] args) 
+  {
+    List<string> vehicles = new List<string>(){ "carro", "moto", "avião" };
+
+    foreach (var item in vehicles)
+    {
+        Console.WriteLine(item);
+    }
+  }
+}
---
+++ linq-train/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace linq_train;
+using System.Linq;
+using System.Linq.Expressions; 
+// using System.Data.Linq; 
+public class Class1
+{
+  public static void Main(string[] args)
+  {
+    GamesControl();
+    string[] actors = { "Al Pacino", "Samuel L. Jackson", "Robert De Niro", "Leonardo DiCaprio", "Morgan Freeman" };
+
+    var actorsList = (from actor
+                        in actors
+                    select actor).Count();
+    // São a mesma coisa...
+    // var actorsList = actors.Count();
+  }
+
+  public static void GamesControl() 
+  {
+    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+
+    // 1° maneira... - declarativeLinq
+    // IEnumerable<string> filteredGames = from game in games
+    //                                          where game.Contains('a')
+    //                                          select game;
+
+    // 2° maneira... - labdaLinq
+    // IEnumerable<string> filteredGames = System.Linq.Enumerable.Where(games, game => game.Contains('a')); 
+
+    // 3° maneira... - labdaLinqRefactored
+    var filteredGames = games.Where(game => game.Contains('a')); 
+
+    foreach (var game in filteredGames)
+    {
+      Console.WriteLine(game);
+    }
+  }
+
+}
