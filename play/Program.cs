42410d33081ce3374f1684fcebc6c36e78566481
Abner <abnerferreira84@gmail.com>
2022-09-27 18:10:09 -0300

    feat: DIO - adicionando exercicio de criação de calculadora

---
+++ play/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CalculatorPlace 
+{
+    public class Calculator
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        public void Potenciacao(int x, int y)
+        {
+            double result = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {result}");
+        }
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 2)}");
+        }
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Conseno de {angulo} = {Math.Round(coseno, 2)}");
+        }
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 2)}");
+        }
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quandrada de {x} = {raiz}");
+        }
+    }
+}
+
---
+++ play/test.cs
@@ -0,0 +1,9 @@
+using CalculatorPlace;
+
+var res = new Calculator();
+
+res.Somar(2, 2);
+res.Potenciacao(2, 3);
+res.Seno(30);
+res.Coseno(30);
+res.Tangente(30);
