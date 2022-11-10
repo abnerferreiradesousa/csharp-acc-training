39430c0b10e266f3219dc664a66ddf3b81a80c5d
Abner <abnerferreira84@gmail.com>
2022-11-10 13:43:43 -0300

    feat: add exercícios de fila, pilha, dictionary e leitura de arquivos externos.

---
+++ pground/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// =================================== Dictionary ======================================
+
+Dictionary<string, string> dict = new()
+{
+    { "SP", "São Paulo" },
+    { "CE", "Ceará" },
+    { "RJ", "Rio Janeiro" },
+    { "PR", "Paraná" }
+};
+
+foreach (KeyValuePair<string, string> item in dict)
+{
+    Console.WriteLine($"Read object {item.Key} - {item.Value}");
+}
+
+dict.Remove("CE");
+
+foreach (KeyValuePair<string, string> item in dict)
+{
+    Console.WriteLine($"Read object {item.Key} - {item.Value}");
+}
+
+
+
+// =================================== Pilha - LIFO =====================================
+
+Stack<string> pilha = new();
+
+pilha.Push("Abner");
+pilha.Push("Bruna");
+pilha.Push("Larissa");
+pilha.Push("Amanda");
+
+foreach (string item in pilha)
+{   
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removeu o elemento: {pilha.Pop()}");
+
+pilha.Push("Filipe");
+
+foreach (string item in pilha)
+{   
+    Console.WriteLine(item);
+}
+
+
+
+
+// =================================== Filas - FIFO ======================================
+
+Queue<string> fila = new();
+
+fila.Enqueue("Abner");
+fila.Enqueue("Bruna");
+fila.Enqueue("Larissa");
+fila.Enqueue("Amanda");
+
+foreach (string item in fila)
+{   
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removeu o elemento: {fila.Dequeue()}");
+
+fila.Enqueue("Filipe");
+
+foreach (string item in fila)
+{   
+    Console.WriteLine(item);
+}
+
+
+
+
+// ================================= Lendo um arquivo ====================================
+
+try
+{
+    string[] text = File.ReadAllLines("FileRead/user.txt");
+
+    foreach (string item in text)
+    {   
+        Console.WriteLine(item);
+    }
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Erro: {ex}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex}");
+}
+finally
+{
+    //Executa mesmo quando lançada uma exceção ou não...
+    Console.WriteLine("Envia um email com o resultado da avaliação.");
+}
